29c2591ab425938799fb7e48db3f5a49bc8ebba8
96Akash <akashkuttan96@gmail.com>
2025-03-18 10:42:08 +0530

    Change

--- server/index.js
+++ server/index.js
@@ -246,13 +246,15 @@ class Program {
   }
 };
 
-app.use(cors({
-  origin: "https://dev-together-blush.vercel.app", // ✅ Allow frontend URL
-  methods: ["GET", "POST"],
-  credentials: true,
-}
-))
-app.use(express.json())
+app.use(cors());
+app.use(express.json({ limit: '1mb' }));
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://dev-together-blush.vercel.app",
+    methods: ["GET", "POST"],
+  },
+});
 
 
 // Map to store room data
